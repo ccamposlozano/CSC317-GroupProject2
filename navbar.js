d1c5231688aba22a6ce56d16e5982f385ef4db7e
CanxinTang <144829399+CanxinTang@users.noreply.github.com>
2024-04-28 19:15:15 -0700

    update Home in nav bar

--- navbar.js
+++ navbar.js
@@ -3,7 +3,7 @@
 document.getElementById('navbar').innerHTML = `
     <nav>
         <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="index.html">Home</a></li>
             <li><a href="#" onclick="toggleCart()">Cart</a></li>
             <li><a href="#" onclick="showUserProfile()">Profile</a></li>
             <li><a href="#" onclick="showCheckoutPage()">Checkout</a></li>
