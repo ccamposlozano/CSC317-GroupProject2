8096575bfe09a8fe1014620fc63c1e0ec8b620d7
ccamposlozano <117617282+ccamposlozano@users.noreply.github.com>
2024-05-30 13:59:42 -0700

    Edit package.json

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', authRoutes);
-app.use('/api', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
